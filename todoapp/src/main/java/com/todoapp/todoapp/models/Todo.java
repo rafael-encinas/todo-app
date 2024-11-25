cf37cc85bab0b1f2cacf91e81fbaa79fe36286d9
Rafael Encinas <rafael.encinas@encora.com>
2024-11-25 09:31:32 -0700

    Remove commented code from 'TodoappApplication.java'

--- todoapp/src/main/java/com/todoapp/todoapp/models/Todo.java
+++ todoapp/src/main/java/com/todoapp/todoapp/models/Todo.java
@@ -1,69 +1,169 @@
 package com.todoapp.todoapp.models;
 
+/**
+ * Represents a Todo item.
+ */
 public class Todo {
-    
-		private int id;
-		private String text;
-		private String dueDate;
-		private boolean doneState;
-		private String doneDate;
-		private int priority;
-		private String creationDate;
+	
+	private int id;
+	private String text;
+	private String dueDate;
+	private boolean doneState;
+	private String doneDate;
+	private int priority;
+	private String creationDate;
 
-		public Todo(int id, String text, String dueDate, boolean doneState, String doneDate, int priority, String creationDate){
-			this.id =id;
-			this.text = text;
-			this.dueDate = dueDate;
-			this.doneState = doneState;
-			this.doneDate = doneDate;
-			this.priority = priority;
-			this.creationDate = creationDate;
-		}
+	/**
+	 * Constructs a Todo object with the specified parameters.
+	 * 
+	 * @param id           the ID of the Todo item
+	 * @param text         the text description of the Todo item
+	 * @param dueDate      the due date of the Todo item
+	 * @param doneState    the completion state of the Todo item
+	 * @param doneDate     the completion date of the Todo item
+	 * @param priority     the priority of the Todo item
+	 * @param creationDate the creation date of the Todo item
+	 */
+	public Todo(int id, String text, String dueDate, boolean doneState, String doneDate, int priority, String creationDate) {
+		this.id = id;
+		this.text = text;
+		this.dueDate = dueDate;
+		this.doneState = doneState;
+		this.doneDate = doneDate;
+		this.priority = priority;
+		this.creationDate = creationDate;
+	}
 
-		public Todo(){
+	/**
+	 * Constructs an empty Todo object.
+	 */
+	public Todo() {
 
-		}
+	}
 
-		public int getId() {
-			return id;
-		}
-		public void setId(int id) {
-			this.id = id;
-		}
-		public String getText() {
-			return text;
-		}
-		public void setText(String text) {
-			this.text = text;
-		}
-		public String getDueDate() {
-			return dueDate;
-		}
-		public void setDueDate(String dueDate) {
-			this.dueDate = dueDate;
-		}
-		public boolean isDoneState() {
-			return doneState;
-		}
-		public void setDoneState(boolean doneState) {
-			this.doneState = doneState;
-		}
-		public String getDoneDate() {
-			return doneDate;
-		}
-		public void setDoneDate(String doneDate) {
-			this.doneDate = doneDate;
-		}
-		public int getPriority() {
-			return priority;
-		}
-		public void setPriority(int priority) {
-			this.priority = priority;
-		}
-		public String getCreationDate() {
-			return creationDate;
-		}
-		public void setCreationDate(String creationDate) {
-			this.creationDate = creationDate;
-		}
+	/**
+	 * Returns the ID of the Todo item.
+	 * 
+	 * @return the ID of the Todo item
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Sets the ID of the Todo item.
+	 * 
+	 * @param id the ID of the Todo item
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * Returns the text description of the Todo item.
+	 * 
+	 * @return the text description of the Todo item
+	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * Sets the text description of the Todo item.
+	 * 
+	 * @param text the text description of the Todo item
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	/**
+	 * Returns the due date of the Todo item.
+	 * 
+	 * @return the due date of the Todo item
+	 */
+	public String getDueDate() {
+		return dueDate;
+	}
+
+	/**
+	 * Sets the due date of the Todo item.
+	 * 
+	 * @param dueDate the due date of the Todo item
+	 */
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	/**
+	 * Returns the completion state of the Todo item.
+	 * 
+	 * @return the completion state of the Todo item
+	 */
+	public boolean isDoneState() {
+		return doneState;
+	}
+
+	/**
+	 * Sets the completion state of the Todo item.
+	 * 
+	 * @param doneState the completion state of the Todo item
+	 */
+	public void setDoneState(boolean doneState) {
+		this.doneState = doneState;
+	}
+
+	/**
+	 * Returns the completion date of the Todo item.
+	 * 
+	 * @return the completion date of the Todo item
+	 */
+	public String getDoneDate() {
+		return doneDate;
+	}
+
+	/**
+	 * Sets the completion date of the Todo item.
+	 * 
+	 * @param doneDate the completion date of the Todo item
+	 */
+	public void setDoneDate(String doneDate) {
+		this.doneDate = doneDate;
+	}
+
+	/**
+	 * Returns the priority of the Todo item.
+	 * 
+	 * @return the priority of the Todo item
+	 */
+	public int getPriority() {
+		return priority;
+	}
+
+	/**
+	 * Sets the priority of the Todo item.
+	 * 
+	 * @param priority the priority of the Todo item
+	 */
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	/**
+	 * Returns the creation date of the Todo item.
+	 * 
+	 * @return the creation date of the Todo item
+	 */
+	public String getCreationDate() {
+		return creationDate;
+	}
+
+	/**
+	 * Sets the creation date of the Todo item.
+	 * 
+	 * @param creationDate the creation date of the Todo item
+	 */
+	public void setCreationDate(String creationDate) {
+		this.creationDate = creationDate;
+	}
 }
